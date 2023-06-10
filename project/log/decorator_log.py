562c7e23af797081229a39cb47b4d132427a4576
TemiralievS <101124591+TemiralievS@users.noreply.github.com>
2023-06-10 12:36:05 +0300

    lesson_6_1 fix decors_logs works (#7)
    lesson_6 decorators and logs ready

--- project/log/decorator_log.py
+++ project/log/decorator_log.py
@@ -4,20 +4,23 @@ import project.log.client_log_config
 import project.log.server_log_config
 import traceback
 import inspect
+from functools import wraps
 
 
 if sys.argv[0].find('client') == -1:
     logger = logging.getLogger('server')
-else:
+elif sys.argv[0].find('client') != -1:
     logger = logging.getLogger('client')
 
 
+
 def log(log_func):
-    def for_logs(*args, **kwargs):
+    @wraps(log_func)
+    def wrapper(*args, **kwargs):
         logs = log_func(*args, **kwargs)
         logger.debug(f'Была вызвана функция {log_func.__name__} c параметрами {args}, {kwargs}. '
                      f'Вызов из модуля {log_func.__module__}. Вызов из'
                      f' функции {traceback.format_stack()[0].strip().split()[-1]}.'
-                     f'Вызов из функции {inspect.stack()[1][3]}')
+                     f'Вызов из функции {inspect.stack()[1][3]}', stacklevel=2)
         return logs
-    return for_logs
+    return wrapper
