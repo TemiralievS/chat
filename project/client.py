ac2b033ba9500d39918281537a979b888a720443
TemiralievS <101124591+TemiralievS@users.noreply.github.com>
2023-06-17 17:50:55 +0300

    Lesson_10 meta and descriptrs (#12)
    lesson_10 Ready

--- project/client.py
+++ project/client.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable, Iterable, Mapping
 import socket
 import sys
 import json
@@ -8,70 +9,110 @@ import threading
 import log.client_log_config
 from log.decorator_log import *
 from errs.errors import *
-from socket import *
 from vars.vars import *
 from funcJson.funcs import *
+from meta import ClientCheck
 
 client_logger = logging.getLogger('client')
 
 
-@log
-def bye_bye_message(account_name):
-    return {
-        jim_action: jim_exit,
-        jim_time: time.time(),
-        jim_account_name: account_name
+class ClientSend(threading.Thread, metaclass=ClientCheck):
+    def __init__(self, account_name, sock):
+        self.account_name = account_name
+        self.sock = sock
+        super().__init__()
+        
+
+    # Функция словарь-сообщение о выходе.
+    def bye_bye_message(self):
+        return {
+            jim_action: jim_exit,
+            jim_time: time.time(),
+            jim_account_name: self.account_name
     }
 
 
-@log
-def message_from_another_client(sock, my_name):
-    while True:
+    # Функция запрашивает адресата и само сообщение аресату, затем отправляет сообщение на сервер.    
+    def client_message_message(self):
+        waiter = input('Кому отправить сообщение: ')
+        message = input('Введите сообщениe: ')
+
+        message_dict = {
+            jim_action: jim_message,
+            jim_sender: self.account_name,
+            jim_waiter: waiter,
+            jim_time: time.time(),
+            jim_message_txt: message
+        }
+        client_logger.debug(f'Сообщение {message_dict}')
         try:
-            message = get_message(sock)
-            if jim_action in message and message[jim_action] == jim_message and \
-                    jim_sender in message and jim_waiter in message and jim_message_txt in message\
-                    and message[jim_waiter] == my_name:
-                print(f'Входящее сообщение от {message[jim_sender]}: {message[jim_message_txt]}')
-                client_logger.info(f'Входящее сообщение от {message[jim_sender]}: {message[jim_message_txt]}')
+            send_message(self.sock, message_dict)
+            client_logger.info(f'Пользователь {waiter} скоро получит ваше сообщение')
+        except:
+            client_logger.critical('Сервер не отвечает')
+            sys.exit(1)
+
+
+    
+    # Функция запрашивает-получает команды от пользователя, отправляет сообщения
+    def run(self):
+        self.print_info()
+        while True:
+            command = input('Что тебе нужно? :')
+            if command == 'message':
+                self.client_message_message()
+            elif command == 'help':
+                self.print_info()
+            elif command == 'exit':
+                try:
+                    send_message(self.sock, self.bye_bye_message())
+                except:
+                    pass
+                print('reservuar')
+                client_logger.info(f'Пользователь покинул чат')
+                time.sleep(1)
+                break
             else:
-                client_logger.error(f'Ошибка чтения сообщения: {message}')
-        except IncorrectDataRecivedError:
-            client_logger.error(f'DECODING ERROR!')
-        except (OSError, ConnectionError, ConnectionAbortedError, ConnectionResetError, json.JSONDecodeError):
-            client_logger.critical(f'Разорвано соединение с сервером')
-            break
+                print('Прочтите еще раз список команд')
 
 
-@log
-def client_message_message(sock, account_name='user'):
-    waiter = input('Кому отправить сообщение: ')
-    message = input('Введите сообщениe: ')
-
-    message_dict = {
-        jim_action: jim_message,
-        jim_sender: account_name,
-        jim_waiter: waiter,
-        jim_time: time.time(),
-        jim_message_txt: message
-    }
-    client_logger.debug(f'Сообщение {message_dict} от {account_name}')
-    try:
-        send_message(sock, message_dict)
-        client_logger.info(f'Пользователь {waiter} скоро получит ваше сообщение')
-    except:
-        client_logger.critical('Сервер не отвечает')
-        sys.exit(1)
+    # Функция показывающая список команд доступных пользователю.
+    def print_info(self):
+        print('Список команд:    ')
+        print('  message - отправить сообщение\n  help - список команд\n  exit-выход')
 
 
+class ClientWait(threading.Thread, metaclass=ClientCheck):
+    def __init__(self, account_name, sock):
+        self.account_name = account_name
+        self.sock = sock
+        super().__init__()
+
+    
+    # Функция принимает сообщение, выводит его в консоль.
+    def run(self):
+        while True:
+            try:
+                message = get_message(self.sock)
+                if jim_action in message and message[jim_action] == jim_message and \
+                        jim_sender in message and jim_waiter in message and jim_message_txt in message\
+                        and message[jim_waiter] == self.account_name:
+                    print(f'Входящее сообщение от {message[jim_sender]}: {message[jim_message_txt]}')
+                    client_logger.info(f'Входящее сообщение от {message[jim_sender]}: {message[jim_message_txt]}')
+                else:
+                    client_logger.error(f'Ошибка чтения сообщения: {message}')
+            except IncorrectDataRecivedError:
+                client_logger.error(f'DECODING ERROR!')
+            except (OSError, ConnectionError, ConnectionAbortedError, ConnectionResetError, json.JSONDecodeError):
+                client_logger.critical(f'Разорвано соединение с сервером')
+                break
+
+
+
+
+# Функция формирует presence-сообщение со стороны клиента
 @log
 def client_message_presence(account_name):
-    '''
-    Функция формирует presence-сообщение со стороны клиента
-    :param account_name:
-    :return: dict
-    '''
-
     out = {
         jim_action: jim_presence,
         jim_time: time.time(),
@@ -83,13 +124,9 @@ def client_message_presence(account_name):
     return out
 
 
+# Функция принимает-разбирает ответ от сервера 
 @log
 def server_message(answer):
-    '''
-    Функция принимает ответ от сервера
-    :param answer:
-    :return:
-    '''
     client_logger.debug(f'Статус сообщения {answer}')
     if jim_response in answer:
         if answer[jim_response] == 200:
@@ -99,40 +136,9 @@ def server_message(answer):
     raise ReqFieldMissingError(jim_response)
 
 
-@log
-def user_commands(sock, user):
-    print_info()
-    while True:
-        command = input('Что тебе нужно? :')
-        if command == 'message':
-            client_message_message(sock, user)
-        elif command == 'help':
-            print_info()
-        elif command == 'exit':
-            send_message(sock, bye_bye_message(user))
-            print('reservuar')
-            client_logger.info(f'Пользователь {user} покинул чат')
-            time.sleep(1)
-            break
-        else:
-            print('Прочтите еще раз список команд')
-
-
-def print_info():
-    print('Список команд:    ')
-    print('  message - отправить сообщение\n  help - список команд\n  exit-выход')
-
-
-
+# Функция, которая забирает(парсит) аргументы из командной строки
 @log
 def arguments_parser():
-    '''
-    Функция, которая забирает аргументы из командной строки
-    :return:
-    server_address - ip-адрес сервера
-    server_port  - порт сервера
-    client_mode - режим, в котором работает клиент
-    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('addr', default='127.0.0.1', nargs='?')
     parser.add_argument('port', default=7777, type=int, nargs='?')
@@ -162,7 +168,7 @@ def main():
         f'порт: {server_port}, имя пользователя: {client_name}')
 
     try:
-        s = socket(AF_INET, SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((server_address, server_port))
         send_message(s, client_message_presence(client_name))
         answer = server_message(get_message(s))
@@ -181,22 +187,21 @@ def main():
         client_logger.critical('Отсутствует разрешение на присоединение')
         sys.exit(1)
     else:
-        receiver = threading.Thread(target=message_from_another_client, args=(s, client_name))
-        receiver.daemon = True
-        receiver.start()
+        module_receiver = ClientWait(client_name, s)
+        module_receiver.daemon = True
+        module_receiver.start()
 
-        user_interface = threading.Thread(target=user_commands, args=(s, client_name))
-        user_interface.daemon = True
-        user_interface.start()
-        client_logger.debug('Запущены процессы')
-
+        module_sender = ClientSend(client_name, s)
+        module_sender.daemon = True
+        module_sender.start()
+        client_logger.debug('Запущено')
+        
         while True:
             time.sleep(1)
-            if receiver.is_alive() and user_interface.is_alive():
+            if module_receiver.is_alive() and module_sender.is_alive():
                 continue
             break
 
 
 if __name__ == '__main__':
     main()
-
